262fc493510c79b0f993b8727fe2c61bf9001698
RafaelBackx <43341347+RafaelBackx@users.noreply.github.com>
2019-10-17 14:01:23 +0200

    first commit

---
+++ Gamble/src/ui/Main.java
@@ -0,0 +1,19 @@
+package ui;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		new ScoreView();
+		PlayerView pv3 = new PlayerView(3);
+		PlayerView pv2 = new PlayerView(2);
+		PlayerView pv1 = new PlayerView(1);		
+		pv1.isAanBeurt(true);
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
