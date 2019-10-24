859481fe620384c334914fb1442e269be4e8dceb
RafaelBackx <43341347+RafaelBackx@users.noreply.github.com>
2019-10-24 10:32:15 +0200

    added player class and factory

--- Gamble/src/domain/Player.java
+++ Gamble/src/domain/Player.java
@@ -1,4 +1,23 @@
 package domain;
 
 public class Player {
+    private Integer playerId;
+    private String name;
+
+    public Player(Integer playerId,String name){
+        this.playerId = playerId;
+        this.name = name;
+    }
+
+    public Integer getPlayerId() {
+        return playerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- Gamble/src/domain/PlayerFactory.java
+++ Gamble/src/domain/PlayerFactory.java
@@ -1,4 +1,11 @@
 package domain;
 
 public class PlayerFactory {
+    public PlayerFactory (){
+
+    }
+
+    public Player createPlayer(Integer playerId,String name){
+        return new Player(playerId,name);
+    }
 }
