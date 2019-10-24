d407c1eb51eab1ca3267d13c654626dc680a6dd4
Tomas <tomadomavicius@gmail.com>
2019-10-24 11:25:39 +0200

    game class

---
+++ Gamble/src/domain/EventType.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum EventType {
+    ADD(new ArrayList<>()),
+    DEPOSIT(new ArrayList<>());
+
+    private List<Observer> observers;
+
+    private EventType(List<Observer> observers){
+        this.observers = observers;
+    }
+
+    public void addObserver(Observer o){
+        this.observers.add(o);
+    }
+
+    public List<Observer> getObservers(){
+        return this.observers;
+    }
+}
--- Gamble/src/domain/Game.java
+++ Gamble/src/domain/Game.java
@@ -1,16 +1,33 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Game {
+public class Game implements Subject{
     private Map<Player, List<Turn>> players;
+    private Map<EventType, List<Observer>> observers;
 
     public Game() {
         players = new HashMap<Player, List<Turn>>();
-
+        observers = new HashMap<EventType, List<Observer>>();
     }
 
+    @Override
+    public void addObserver(EventType e, Observer o) {
+        observers.get(e).add(o);
+    }
 
+    @Override
+    public void removeObserver(EventType e, Observer o) {
+        this.observers.get(e).remove(o);
+    }
+
+    @Override
+    public void notifyObserver(EventType e, String s) {
+        for (Observer o:this.observers.get(e)) {
+            o.update(s);
+        }
+    }
 }
