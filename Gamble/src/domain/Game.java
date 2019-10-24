b99f1c23418ed235b47bb929a9314d9a754c5416
Tomas <tomadomavicius@gmail.com>
2019-10-24 11:04:11 +0200

    game class

--- Gamble/src/domain/Game.java
+++ Gamble/src/domain/Game.java
@@ -1,5 +1,16 @@
 package domain;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Game {
+    private Map<Player, List<Turn>> players;
+
+    public Game() {
+        players = new HashMap<Player, List<Turn>>();
+
+    }
+
 
 }
